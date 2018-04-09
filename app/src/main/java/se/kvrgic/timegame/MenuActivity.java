71d8f7ccd6d3397911c2bbffac7f6e7b32165ca8
Srdan Kvrgic <gitkraken@kvrgic.se>
2018-04-09 13:20:04 +0200

    Viii... Vi börjar väl logga innan vi förstör alltför mycket, visst?

---
+++ app/src/main/java/se/kvrgic/timegame/MenuActivity.java
@@ -0,0 +1,24 @@
+package se.kvrgic.timegame;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void doEasyGame(View view) {
+        startActivity(new Intent(this, Game1Activity.class));
+    }
+
+    public void doHardGame(View view) {
+
+    }
+}
