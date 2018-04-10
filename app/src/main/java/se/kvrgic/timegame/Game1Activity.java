68c2c18d08082615ea2e842bbc919081bc332942
Srdan Kvrgic <gitkraken@kvrgic.se>
2018-04-10 13:05:48 +0200

    Ritar ut klockslaget

--- app/src/main/java/se/kvrgic/timegame/Game1Activity.java
+++ app/src/main/java/se/kvrgic/timegame/Game1Activity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class Game1Activity extends Activity {
 
@@ -14,9 +15,21 @@ public class Game1Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game1);
+        doSetupGame();
     }
 
     public void doAcceptAnswer(View view) {
         Log.d(TAG, "doAcceptAnswer: ");
     }
+
+
+
+    private void doSetupGame() {
+        long hour = Math.round(Math.random() * 12);
+        float hourAngle = hour * 360f / 12;
+        View hour_hand = findViewById(R.id.game1_hour);
+        hour_hand.setRotation(hourAngle);
+        Toast.makeText(this, "Hour: " + hour + ", angle: " + hourAngle, Toast.LENGTH_SHORT).show();
+    }
+
 }
