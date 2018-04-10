24e753262c2fa5f7d54e1c0dd5e0107640d5f3bc
Srdan Kvrgic <gitkraken@kvrgic.se>
2018-04-10 12:52:10 +0200

    Acceptera svaret

--- app/src/main/java/se/kvrgic/timegame/Game1Activity.java
+++ app/src/main/java/se/kvrgic/timegame/Game1Activity.java
@@ -3,12 +3,20 @@ package se.kvrgic.timegame;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Game1Activity extends Activity {
+
+    public static final String TAG = "Game1Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game1);
+    }
+
+    public void doAcceptAnswer(View view) {
+        Log.d(TAG, "doAcceptAnswer: ");
     }
 }
